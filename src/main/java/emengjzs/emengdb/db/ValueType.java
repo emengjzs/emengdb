b69d2431d5b3e7d7d6dcc136925a293656c9c3c4
emengjzs <emengjzs@live.com>
2016-12-21 23:47:41 +0800

    Add 'equals' method

--- src/main/java/emengjzs/emengdb/db/ValueType.java
+++ src/main/java/emengjzs/emengdb/db/ValueType.java
@@ -8,11 +8,27 @@ package emengjzs.emengdb.db;
  * Created by emengjzs on 2016/8/30.
  */
 public enum ValueType {
-    DELETE,
-    VALUE;
 
-    byte toByte() {
-        return (byte) this.ordinal();
+    DELETE(0x00),
+    VALUE(0x01),
+    UNKNOWN(0x02);
+
+    final byte v;
+
+    ValueType(int v) {
+        this.v = (byte) v;
     }
 
+    static ValueType of(int i) {
+        return i > ValueType.values().length ? UNKNOWN : ValueType.values()[i];
+    }
+
+
+    byte toByte() {
+        return v;
+    }
+
+    boolean equals(byte v) {
+        return this.v == v;
+    }
 }
